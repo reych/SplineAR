58aa025dd931b1a8c700d67e774a21e66ec14d58
Rena Chen <rena.chen@bcgdv.com>
2017-08-07 13:45:19 -0700

    first commit

---
+++ SplineAR/CRSpline.swift
@@ -0,0 +1,194 @@
+//
+//  CRSpline.swift
+//  SplineAR
+//
+//  Created by Chen, Rena on 8/7/17.
+//  Copyright © 2017 Chen, Rena. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+typealias Point = vector_float3
+
+class CRSpline: NSObject {
+    enum EditAction {
+        case move
+        case delete
+    }
+    
+    // List of control points
+    var controlPoints = [SCNNode]()
+    
+    // Map of start CV (p1) to segment. Each segment is an array of positions. Segment 0 begins at CV 1.
+    var segments = [SCNNode: [SCNNode]]()
+    
+    // Tension parameter
+    var s: Float = 0.5
+    
+    // Basis matrix, determined from tension parameter s.
+    var basis: matrix_float4x4 {
+        return matrix_float4x4(float4(-s, 2*s, -s, 0), float4(2-s, s-3, 0, 1), float4(s-2, 3-2*s, s, 0), float4(s, -s, 0, 0))
+    }
+    
+    // Parameter interval
+    var t: Float = 0.1
+    
+    override init() {
+        super.init()
+    }
+    
+    // Loop through all control points and draw spline.
+    func constructSpline() {
+        //positions = [SCNNode]()
+        for index in 1..<controlPoints.count-2 {
+            print("\(index)")
+            let p0 = controlPoints[index-1]
+            let p1 = controlPoints[index]
+            let p2 = controlPoints[index+1]
+            let p3 = controlPoints[index+2]
+            
+            // Calculate the points between control points.
+            constructSplineSegment(p0: p0, p1: p1, p2: p2, p3: p3, parameterInterval: t)
+        }
+    }
+    
+    // Optimization: draw only the newest segment.
+    // Return index of start of new nodes.
+    func constructNewestSegment() -> Int {
+        guard controlPoints.count >= 4 else {
+            return -1
+        }
+        let index = controlPoints.count - 3
+        constructSplineSegment(p0: controlPoints[index-1], p1: controlPoints[index], p2: controlPoints[index+1], p3: controlPoints[index+2], parameterInterval: t)
+        return index
+    }
+    
+    // Draw the segment between two control points
+    func constructSplineSegment(p0: SCNNode, p1: SCNNode, p2: SCNNode, p3: SCNNode, parameterInterval: Float) {
+        segments[p1] = [SCNNode]() // Initialize segment array
+        
+        let controlMatrix = getControlMatrix(p0: p0, p1: p1, p2: p2, p3: p3)
+        var u:Float = 0
+        while u < 1 {
+            let parameterVector = vector4(u*u*u, u*u, u, 1)
+            let translate = parameterVector * basis * controlMatrix
+            
+            // create node.
+            let g = SCNSphere(radius: 0.005)
+            let splinePoint = SCNNode(geometry: g)
+            splinePoint.position = SCNVector3(translate.x, translate.y, translate.z)
+            
+            // Add node to positions.
+            //positions.append(splinePoint)
+            segments[p1]?.append(splinePoint) // Add to segment.
+            
+            u += parameterInterval
+        }
+    }
+    
+    func getControlMatrix(p0: SCNNode, p1: SCNNode, p2: SCNNode, p3: SCNNode) -> matrix_float3x4{
+        let p0_trans = p0.simdTransform.columns.3
+        let p1_trans = p1.simdTransform.columns.3
+        let p2_trans = p2.simdTransform.columns.3
+        let p3_trans = p3.simdTransform.columns.3
+        // COLUMN MAJOR IS THIS THE RIGHT ORDER??
+        return matrix_float3x4(float4(p0_trans.x, p1_trans.x, p2_trans.x, p3_trans.x), float4(p0_trans.y, p1_trans.y, p2_trans.y, p3_trans.y), float4(p0_trans.z, p1_trans.z, p2_trans.z, p3_trans.z))
+    }
+    
+    func addControlPoint(_ point: SCNNode) {
+        controlPoints.append(point)
+    }
+    
+    // For now, just reconstruct the spline and redo positions array
+    func removeControlPoint(_ node: SCNNode) -> [Int]{
+        let index = controlPoints.index(of: node) as! Int
+        print("deleting index: \(index)")
+        
+        controlPoints.remove(at: index)
+        segments[node] = nil
+        
+        return rebuildAffectedSegments(index: index, editAction: .delete)
+        
+    }
+    
+    func moveControlPoint(_ point: SCNNode, position: SCNVector3) -> [Int] {
+        point.position = position
+        let index = controlPoints.index(of: point) as! Int
+        
+        // Find all affected segments and recalculate.
+        return rebuildAffectedSegments(index: index, editAction: .move)
+    }
+    
+    // Return true if it's a control point in the spline.
+    func isControlPoint(_ node: SCNNode) -> Bool{
+        for point in controlPoints {
+            if node == point {
+                return true
+            }
+        }
+        return false
+    }
+    
+    // Return control points that begin the affected segments.
+    func getAffectedSegmentsOfControlPoint(_ point: SCNNode, editAction: EditAction) -> [[SCNNode]]{
+        let index = controlPoints.index(of: point) as! Int
+        var nodes = [[SCNNode]]()
+        // Check first segment.
+        if index - 3 >= 0 && index < controlPoints.count {
+            let points = segments[controlPoints[index-2]]
+            nodes.append(points!)
+        }
+        // Second segment.
+        if index - 2 >= 0 && index + 1 < controlPoints.count {
+            let points = segments[controlPoints[index-1]]
+            if let p = points {
+                nodes.append(p)
+            }
+        }
+        // Third segment.
+        if index - 1 >= 0 && index + 2 < controlPoints.count{
+            let points = segments[controlPoints[index]]
+            if let p = points {
+                nodes.append(p)
+            }
+        }
+        // Fourth segment.
+        if index >= 0 && index + 3 < controlPoints.count && editAction == EditAction.move{
+            let points = segments[controlPoints[index+1]]
+            if let p = points {
+                nodes.append(p)
+            }
+        }
+        print("affected segments: \(nodes.count)")
+        return nodes
+    }
+    
+    private func rebuildAffectedSegments(index: Int, editAction: EditAction) -> [Int]{
+        var indices = [Int]()
+        // Check first segment.
+        if index - 3 >= 0 && index < controlPoints.count {
+            indices.append(index-2)
+            constructSplineSegment(p0: controlPoints[index-3], p1: controlPoints[index-2], p2: controlPoints[index-1], p3: controlPoints[index], parameterInterval: t)
+        }
+        // Second segment.
+        if index - 2 >= 0 && index + 1 < controlPoints.count {
+            indices.append(index-1)
+            constructSplineSegment(p0: controlPoints[index-2], p1: controlPoints[index-1], p2: controlPoints[index], p3: controlPoints[index+1], parameterInterval: t)
+        }
+        // Third segment.
+        if index - 1 >= 0 && index + 2 < controlPoints.count{
+            indices.append(index)
+            constructSplineSegment(p0: controlPoints[index-1], p1: controlPoints[index], p2: controlPoints[index+1], p3: controlPoints[index+2], parameterInterval: t)
+        }
+        // Fourth segment.
+        if index >= 0 && index + 3 < controlPoints.count && editAction == EditAction.move{
+            indices.append(index+1)
+            constructSplineSegment(p0: controlPoints[index], p1: controlPoints[index+1], p2: controlPoints[index+2], p3: controlPoints[index+3], parameterInterval: t)
+        }
+        return indices
+    }
+    
+    
+}
+
--- SplineAR/ViewController.swift
+++ SplineAR/ViewController.swift
@@ -11,8 +11,20 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    var spline = CRSpline()
+    
+    //var pointNodes = [SCNNode]()
+    //var controlNodes: Set = Set<SCNNode>()
+    //var controlAnchors = [ARAnchor]()
+    
+    var highlightNode: SCNNode! // For UI indication
+    var selectedNode: SCNNode? // Selected node for interaction.
+    
+    var vectorToNode: vector_float3?
+    
+    var touchLocation: CGPoint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +40,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        setupObjects()
+        setupGestures()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -51,17 +66,238 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
+    
+    // MARK: - Setup
+    func setupObjects() {
+        let g = SCNSphere(radius: 0.01)
+        g.firstMaterial?.transparency = 0.5
+        g.firstMaterial?.diffuse.contents = UIColor.orange.cgColor
+        highlightNode = SCNNode(geometry: g)
+        sceneView.scene.rootNode.addChildNode(highlightNode)
+    }
+    
+    func setupGestures() {
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressed(_:)))
+        let singleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(singleTap(_:)))
+        singleTapRecognizer.numberOfTapsRequired = 1
+        let swipeUpRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeUp(_:)))
+        swipeUpRecognizer.direction = .up
+        
+        
+        self.view.addGestureRecognizer(longPressRecognizer)
+        self.view.addGestureRecognizer(singleTapRecognizer)
+        self.view.addGestureRecognizer(swipeUpRecognizer)
+    }
+    
+    // MARK: - Handle touch event.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touches began")
+        // Select node
+        if let location = touches.first?.location(in: sceneView) {
+            touchLocation = location
+        }
+    }
+    
+    // Long press event.
+    @objc func longPressed(_ sender: UIGestureRecognizer) {
+        print("longpressed")
+        switch sender.state {
+        case .began:
+            print("began")
+            // Return if can't get touch location (this gets set in touches began, it shouldn't ever be nil)
+            guard touchLocation != nil else {
+                return
+            }
+            // Hit test to check intersection and select node.
+            if let hit = sceneView.hitTest(touchLocation!, options: nil).first {
+                //hit.node.removeFromParentNode()
+                if selectedNode == hit.node {
+                    deselectNode()
+                } else {
+                    selectNode(hit.node)
+                }
+                
+                return
+            }
+            
+        default:
+            print("default")
+        }
+        
+    }
+    
+    // Single tap.
+    @objc func singleTap(_ sender: UIGestureRecognizer) {
+        print("single tap")
+        
+        // Did not hit node, so deselect node.
+        if selectedNode != nil {
+            deselectNode()
+            return
+        }
+        
+        
+        // Add control point node
+        if let currentFrame = sceneView.session.currentFrame {
+            var translateMatrix = matrix_identity_float4x4
+            translateMatrix.columns.3.z = -0.15
+            let transform = simd_mul(currentFrame.camera.transform, translateMatrix)
+            
+            // Create new control point and add it to spline.
+            var translateVector = transform.columns.3
+            //let point = Point(translateVector.x, translateVector.y, translateVector.z)
+            let g = SCNSphere(radius: 0.01)
+            g.firstMaterial?.diffuse.contents = UIColor.red.cgColor
+            let controlPoint = SCNNode(geometry: g)
+            controlPoint.position = SCNVector3(translateVector.x, translateVector.y, translateVector.z)
+            spline.addControlPoint(controlPoint)
+            sceneView.scene.rootNode.addChildNode(controlPoint)
+            
+            // Construct newest segment if have enough control points.
+            let cvIndex = spline.constructNewestSegment()
+            if cvIndex > -1 {
+                let segment = spline.segments[spline.controlPoints[cvIndex]]
+                for point in segment! {
+                    sceneView.scene.rootNode.addChildNode(point)
+                }
+            }
+            
+        }
+    }
+    
+    // Swipe up.
+    @objc func swipeUp(_ sender: UIGestureRecognizer) {
+        print ("swipe up")
+        // Display options for selected node.
+        if let node = selectedNode {
+            let actionSheetController = UIAlertController(title: "Options", message: "Choose an action", preferredStyle: .actionSheet)
+            let deleteActionButton = UIAlertAction(title: "Delete", style: .destructive, handler: {_ in
+                print("delete")
+                self.deselectNode()
+                //Get affected segments and delete. Edit action is 'move' because node is not yet deleted.
+                let affectedSegments = self.spline.getAffectedSegmentsOfControlPoint(node, editAction: .move)
+                
+                for segment in affectedSegments {
+                    for point in segment {
+                        point.removeFromParentNode()
+                    }
+                }
+                print("affected segments num: \(affectedSegments.count)")
+                
+                node.removeFromParentNode()
+                let cvIndices = self.spline.removeControlPoint(node)
+                
+                // Update the intermediate points.
+                for index in cvIndices {
+                    let controlPoint = self.spline.controlPoints[index]
+                    if let segment = self.spline.segments[controlPoint] {
+                        for point in segment {
+                            self.sceneView.scene.rootNode.addChildNode(point)
+                        }
+                    }
+                }
+                
+                
+            })
+            let cancelActionButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            actionSheetController.addAction(deleteActionButton)
+            actionSheetController.addAction(cancelActionButton)
+            self.present(actionSheetController, animated: true, completion: nil)
+        }
+    }
+    
+    // Update position of highlight node.
+    func updateHighlightNode() {
+        if let currentFrame = sceneView.session.currentFrame {
+            var translateMatrix = matrix_identity_float4x4
+            translateMatrix.columns.3.z = -0.15
+            let transform = simd_mul(currentFrame.camera.transform, translateMatrix)
+            highlightNode.simdTransform = transform
+        }
+    }
+    
+    func updateSelectedNode() {
+        if let node = selectedNode {
+            if let currentFrame = sceneView.session.currentFrame {
+                
+                var translateMatrix = matrix_identity_float4x4
+                translateMatrix.columns.3.z = -0.15
+                
+                let transform = simd_mul(currentFrame.camera.transform, translateMatrix)
+                
+                // Delete old nodes.
+                let affectedSegments = spline.getAffectedSegmentsOfControlPoint(node, editAction: .move)
+                
+                for segment in affectedSegments {
+                    for point in segment {
+                        point.removeFromParentNode()
+                    }
+                }
+                
+                // Move node.
+                let cvIndices = spline.moveControlPoint(node, position: SCNVector3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z))
+                // Update the intermediate points.
+                for index in cvIndices {
+                    let controlPoint = spline.controlPoints[index]
+                    if let segment = spline.segments[controlPoint] {
+                        for point in segment {
+                            sceneView.scene.rootNode.addChildNode(point)
+                        }
+                    }
+                }
+                //node.position = SCNVector3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+                
+                //print(node.simdTransform.columns.3)
+            }
+        }
+    }
+    
+    // MARK: - Node selection logic
+    func selectNode(_ node: SCNNode) {
+        if spline.isControlPoint(node) {
+            deselectNode()
+            selectedNode = node
+            selectedNode?.geometry?.firstMaterial?.diffuse.contents = UIColor.cyan.cgColor
+            
+            if let currentFrame = sceneView.session.currentFrame {
+                let nodePos = node.position
+                let camPos = currentFrame.camera.transform.columns.3
+                vectorToNode = vector3(nodePos.x - camPos.x, nodePos.y - camPos.y, nodePos.z - camPos.z)
+                print(vectorToNode!)
+            }
+        }
+    }
+    
+    func deselectNode() {
+        if let node = selectedNode {
+            node.geometry?.firstMaterial?.diffuse.contents = UIColor.red.cgColor
+            selectedNode = nil
+        }
+    }
+    
+    // Return true if is a node for a control point.
+    func isControlPointNode(_ node: SCNNode) {
+        
+    }
 
     // MARK: - ARSCNViewDelegate
     
-/*
+    // Update stuff.
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        updateHighlightNode()
+        updateSelectedNode()
+    }
+    
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
+        let g = SCNSphere(radius: 0.01)
+        g.firstMaterial?.diffuse.contents = UIColor.red.cgColor
+        let node = SCNNode(geometry: g)
+        
+        node.position = SCNVector3Zero
      
         return node
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
